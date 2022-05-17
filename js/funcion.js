770b748d1cab63830fdea261354fa347c244faba
Ginto11 <salinitosnelson@gmail.com>
2022-05-16 22:50:22 -0500

    Subida Actualizacion

--- js/funcion.js
+++ js/funcion.js
@@ -1,5 +1,52 @@
 const $template = document.getElementById("template").content
 const $fragmento = document.createDocumentFragment()
+
+tarjetas = []
+
+
+class Tarjeta{
+    static id = 0
+    constructor(titulo, imagen, costo, descripcion){
+        this.nombre = titulo;
+        this.imagen = imagen;
+        this.descripcion = descripcion; 
+        this.costo = costo;
+        this.id = Tarjeta.id
+        Tarjeta.id++
+    }
+
+    crearTajetas(){
+        $template.querySelector("h1").textContent = this.nombre;
+        $template.querySelector("img").setAttribute("src", this.imagen);
+        $template.querySelector("img").setAttribute("alt", "imagen");
+        $template.querySelector("p").textContent = this.descripcion;
+        $template.querySelector("h2").textContent = "Costo: " + this.costo
+        $template.querySelector(".card").dataset.id = this.id
+        $template.querySelector("button").dataset.id = this.id
+        let $clone = document.importNode($template, true);
+        $fragmento.appendChild($clone)
+    }
+
+    static comprar(id){
+        tarjetas.map(item =>{
+            if(item.id == id){
+                console.log(item)
+                let confirmar = confirm("Deseas comprar el elemento?");
+                
+                if(confirmar){
+                    let costo = parseInt(prompt("Ingresa el valor?"));
+                    if(!costo) return alert("Ingrese caracteres validos");
+                    (item.costo > costo) ? alert("Sin fondos suficientes")
+                        : alert("Comprado con exito");
+                        
+                }
+            }
+        })
+        
+    }
+
+}
+
 card = [
     { nombre: "Guantes", img: "./img/guantes.png", costo: 30000, descripcion: "Guantes para cicla de color negro y material en cuero" },
     { nombre: "Gorro", img: "./img/gorro.png", costo: 15000, descripcion: "Gorro en tela, de color gris y de marca nismo" },
@@ -8,29 +55,68 @@ card = [
     { nombre: "Tenis", img: "./img/tenis.png", costo: 120000, descripcion: "Tenis de color azul y blanco, marca nike, cordones de color negro, con garantia de 2 meses"},
     { nombre: "Reloj", img: "./img/reloj.png", costo: 80000, descripcion: "Reloj de color rosado, con medidor de temperatura, distancia, duracion, material metalico."},
     { nombre: "Mouse", img: "./img/mouse.png", costo: 25000, descripcion: "Mouse optico de color negro tipo gamer, con luces de colores"},
-    { nombre: "Celular", img: "./img/celular.png", costo: 800000, descripcion: "Celular Samsung A3, con 8GB RAM, android 11, 164 GB de almacenamiento interno y con manos libre" }
+    { nombre: "Celular", img: "./img/celular.png", costo: 800000, descripcion: "Celular Samsung A3, con 8GB RAM, android 11, 164 GB de almacenamiento interno y con manos libre" },
+    { nombre: "Portatil", img: "./img/portatil.png", costo: 1800000, descripcion: "Portatil intel core 5, 500GB de disco duro, 4 GB RAM, windows 10 pro"}
+
 ];
 
-card.forEach(element => {
-    $template.querySelector("h1").textContent = element.nombre;
-    $template.querySelector("img").setAttribute("src", element.img);
-    $template.querySelector("img").setAttribute("alt", "imagen");
-    $template.querySelector("p").textContent = element.descripcion;
-    $template.querySelector("h2").textContent = "Costo: " + element.costo
 
-    let $clone = document.importNode($template, true);
-    $fragmento.appendChild($clone)
-});
+// card.forEach(element => {
+//     $template.querySelector("h1").textContent = element.nombre;
+//     $template.querySelector("img").setAttribute("src", element.img);
+//     $template.querySelector("img").setAttribute("alt", "imagen");
+//     $template.querySelector("p").textContent = element.descripcion;
+//     $template.querySelector("h2").textContent = "Costo: " + element.costo
 
+//     let $clone = document.importNode($template, true);
+//     $fragmento.appendChild($clone)
+// });
+card.forEach(elemt =>{
 
+    const tarjeta = new Tarjeta(elemt.nombre, elemt.img, elemt.costo, elemt.descripcion)
+    tarjeta.crearTajetas()
+    tarjetas.push(tarjeta)
+})
 document.querySelector(".contenedor__tarjetas").appendChild($fragmento);
+
 
 const btn = document.getElementById("btn__cambiar--color");
 
 btn.addEventListener("click", ()=>{
     btn.classList.toggle("icon-sol");
     btn.classList.toggle("icon-luna")
+});
+
+document.addEventListener("click", e =>{
+    console.log(e.target)
+    if(e.target.dataset.id){
+        console.log(e.target.dataset.id)
+        
+        if(e.target.matches("button")){
+            
+            tarjetas.map((item) =>{
+
+                if(item.id == e.target.dataset.id){
+                    let id = e.target.dataset.id
+                    Tarjeta.comprar(id)
+                }
+            })
+        }
+    }
 })
 
+/* document.getElementById("btnAgregar").addEventListener("click", ()=>{
+    nombre = prompt("ingrese el nombre")
+    imagen = prompt("ingrese la ruta")
+    costo = parseInt(prompt("Ingrese el costo"))
+    descripcion = prompt("ingrese la descripcion")
 
+    card.push({nombre, imagen, costo, descripcion})
 
+    card.forEach(elemt =>{
+
+        const tarjeta = new Tarjeta(elemt.nombre, elemt.img, elemt.costo, elemt.descripcion)
+        tarjeta.crearTajetas()
+    })
+})
+ */
---
+++ js/iniciar_sesion.js
@@ -0,0 +1,66 @@
+const $formulario_Registro = document.getElementById("formulario__registro");
+const $formulario_Sesion = document.getElementById("formulario__sesion");
+const $btnRegistro = document.getElementById("btn__registrarse")
+const $btnLogearme = document.getElementById("btn__logearse");
+
+listaUsuarios = [
+    { correo: "empresanvs@gmail.com", password: 1001944317 }
+]
+
+
+$formulario_Sesion.addEventListener("submit", e =>{
+    e.preventDefault()
+
+    const datos = new FormData($formulario_Sesion);
+    const [correo, password] = [...datos.values()];
+
+    listaUsuarios.forEach(element =>{
+        document.getElementById("correo").value = ""
+        document.getElementById("password").value = ""
+        if(element.correo == correo && element.password != password) return alert("Contraseña incorrecta");
+        if(element.correo == correo && element.password == password) return setTimeout("location.href = './productos.html'");
+    })
+});
+
+$formulario_Registro.addEventListener("submit", e =>{
+    e.preventDefault()
+
+    const datos1 = new FormData($formulario_Registro);
+    const [correo1, password1] = [...datos1.values()]
+
+    listaUsuarios.forEach(element =>{
+        if(element.correo !== correo1){
+            
+            document.getElementById("correo2").value = ""
+            document.getElementById("password2").value = ""
+            alert("Usuario registrado exitosamente")
+            listaUsuarios.push({correo: correo1, password: password1})
+            crearFrameLogin();
+        }else{
+            alert("Este usuario ya se encuentra registrado")
+        }
+    })
+})
+
+$btnRegistro.addEventListener("click", ()=>{
+    crearFrameRegistro()
+})
+
+$btnLogearme.addEventListener("click", ()=>{
+    crearFrameLogin()
+})
+
+
+function crearFrameRegistro(){
+    $formulario_Sesion.style.display = "none"
+    $formulario_Registro.style.display = "flex"
+    $btnLogearme.style.display = "flex"
+    $btnRegistro.style.display = "none"
+}
+
+function crearFrameLogin(){
+    $formulario_Registro.style.display = "none"
+    $btnRegistro.style.display = "flex"
+    $btnLogearme.style.display = "none"
+    $formulario_Sesion.style.display = "flex"
+}
